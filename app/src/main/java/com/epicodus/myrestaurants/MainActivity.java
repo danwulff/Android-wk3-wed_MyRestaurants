caf908c133782cfa89724273a1d5c45987f095ae
Daniel Wulff and Nadiya Yeroshkina <daniel@>
2016-06-28 08:31:43 -0700

    added font

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.myrestaurants;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +19,17 @@ public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
 
+    private TextView mAppNameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //custom font
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mAppNameTextView.setTypeface(pacifico);
 
         mLocationEditText = (EditText) findViewById(R.id. locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
